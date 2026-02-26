2d8c6e367bd5bacf33a257d22a25d1376f300d86
Weekly_Weather <an23si@brocku.ca>
2026-02-26 16:31:40 -0500

    New stuff ig

--- PythonProject7/main.py
+++ PythonProject7/main.py
@@ -7,31 +7,31 @@ import seaborn as sns
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings
-
-
-DataFrame = pd.read_csv('team17_callcenter.csv')
-#finding out the resolution rate by call type
-resolved = DataFrame.groupby('call_type')['resolved'].value_counts(normalize=True)
-print_line(resolved)
-
-
-#Finding the which time of day is the longest
-groupByDay = DataFrame.groupby('time_of_day')['duration_minutes'].mean()
-print_line(groupByDay)
-
-#finding the percentage of resolved high priority calls
-highPriority = DataFrame.groupby('priority')['resolved'].value_counts(normalize=True)*100
-print_line(highPriority)
-
-#Looking at how agent work load varies by time of day
-
-
-#Spotting any missing values in our dataFrame:
-isNull = DataFrame.isnull().sum()
-print_line(isNull)
-#Getting the percentages of the null variables
-isNull = DataFrame.isnull().mean()*100
-print_line(isNull)
+#
+#
+# DataFrame = pd.read_csv('team17_callcenter.csv')
+# #finding out the resolution rate by call type
+# resolved = DataFrame.groupby('call_type')['resolved'].value_counts(normalize=True)
+# print_line(resolved)
+#
+#
+# #Finding the which time of day is the longest
+# groupByDay = DataFrame.groupby('time_of_day')['duration_minutes'].mean()
+# print_line(groupByDay)
+#
+# #finding the percentage of resolved high priority calls
+# highPriority = DataFrame.groupby('priority')['resolved'].value_counts(normalize=True)*100
+# print_line(highPriority)
+#
+# #Looking at how agent work load varies by time of day
+#
+#
+# #Spotting any missing values in our dataFrame:
+# isNull = DataFrame.isnull().sum()
+# print_line(isNull)
+# #Getting the percentages of the null variables
+# isNull = DataFrame.isnull().mean()*100
+# print_line(isNull)
 
 
 #finding out the resolution rate by call type
@@ -47,3 +47,5 @@ print_line(groupByDay)
 #finding the percentage of resolved high priority calls
 highPriority = ModifiedDataFrame.groupby('priority')['resolved'].value_counts(normalize=True)*100
 print_line(highPriority)
+
+#Dtermining Agent Workload varied by time(per agent id)
