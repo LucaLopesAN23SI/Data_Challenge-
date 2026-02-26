90a96e8c1f0d8b6d394d210e58523222447c7545
Weekly_Weather <an23si@brocku.ca>
2026-02-26 17:10:24 -0500

    sure man

--- PythonProject7/main.py
+++ PythonProject7/main.py
@@ -3,7 +3,6 @@ from ftplib import print_line
 
 import pandas as pd
 import numpy as np
-<<<<<<< Updated upstream
 import seaborn as sns
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -34,8 +33,6 @@ print_line(isNull)
 isNull = DataFrame.isnull().mean()*100
 print_line(isNull)
 
-=======
->>>>>>> Stashed changes
 
 #finding out the resolution rate by call type
 ModifiedDataFrame = pd.read_csv('team17_callcenter_modified.csv')
@@ -49,8 +46,5 @@ print_line(groupByDay)
 
 #finding the percentage of resolved high priority calls
 highPriority = ModifiedDataFrame.groupby('priority')['resolved'].value_counts(normalize=True)*100
-<<<<<<< Updated upstream
 print_line(highPriority)
-=======
 print_line(highPriority)
->>>>>>> Stashed changes
