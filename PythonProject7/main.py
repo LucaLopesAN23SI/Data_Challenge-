fde01f3610985958b5b9ef7c0a5b28e4c451a30d
Weekly_Weather <an23si@brocku.ca>
2026-02-26 15:47:45 -0500

    Modified Data Frame Code

--- PythonProject7/main.py
+++ PythonProject7/main.py
@@ -10,18 +10,18 @@ import seaborn as sns
 
 
 DataFrame = pd.read_csv('team17_callcenter.csv')
-# #finding out the resolution rate by call type
-# resolved = DataFrame.groupby('call_type')['resolved'].value_counts(normalize=True)
-# print_line(resolved)
-#
-#
-# #Finding the which time of day is the longest
-# groupByDay = DataFrame.groupby('time_of_day')['duration_minutes'].mean()
-# print_line(groupByDay)
-#
-# #finding the percentage of resolved high priority calls
-# highPriority = DataFrame.groupby('priority')['resolved'].value_counts(normalize=True)*100
-# print_line(highPriority)
+#finding out the resolution rate by call type
+resolved = DataFrame.groupby('call_type')['resolved'].value_counts(normalize=True)
+print_line(resolved)
+
+
+#Finding the which time of day is the longest
+groupByDay = DataFrame.groupby('time_of_day')['duration_minutes'].mean()
+print_line(groupByDay)
+
+#finding the percentage of resolved high priority calls
+highPriority = DataFrame.groupby('priority')['resolved'].value_counts(normalize=True)*100
+print_line(highPriority)
 
 #Looking at how agent work load varies by time of day
 
@@ -32,3 +32,18 @@ print_line(isNull)
 #Getting the percentages of the null variables
 isNull = DataFrame.isnull().mean()*100
 print_line(isNull)
+
+
+#finding out the resolution rate by call type
+ModifiedDataFrame = pd.read_csv('team17_callcenter_modified.csv')
+resolved = ModifiedDataFrame.groupby('call_type')['resolved'].value_counts(normalize=True)
+print_line(resolved)
+
+
+#Finding the which time of day is the longest
+groupByDay = ModifiedDataFrame.groupby('time_of_day')['duration_minutes'].mean()
+print_line(groupByDay)
+
+#finding the percentage of resolved high priority calls
+highPriority = ModifiedDataFrame.groupby('priority')['resolved'].value_counts(normalize=True)*100
+print_line(highPriority)
